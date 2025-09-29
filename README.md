92b2298c6f134e3847d880d4296fde0f409a3f58
Kushagra Sharma <sparrow.kushagra@gmail.com>
2025-09-29 20:56:48 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TDACorp - Editor
+
+A creative kitchen for crafting beautiful web pages; a whole new experience for content editors to design and publish content effortlessly.
---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next"
+import "@/styles/globals.css"
+import { geistMono, geistSans } from "@/lib/fonts"
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - TDACorp",
+    default: "Dashboard - TDACorp",
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
