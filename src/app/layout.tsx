77d620f889d87f5fa882e77bffe8c33f2f4c6909
mohitrajgarh <mohitsainirajgarh@gmail.com>
2025-10-01 15:27:40 +0530

    fix(client): make it client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import "@/styles/globals.css"
 import { geistMono, geistSans } from "@/lib/fonts"
-import Sliderbar from "@/components/Sliderbar"
 
 export const metadata: Metadata = {
   title: {
--- src/components/Sliderbar.tsx
+++ src/components/Sliderbar.tsx
@@ -1,4 +1,5 @@
-export default function () {
+ "use client"
+export default function Sliderbar() {
    const topices = [
         "Dashboard",
         "Posts",
