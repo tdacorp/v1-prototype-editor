df1450471a173726a12e9195caa469e2c06d6dbb
hritik saini <sainihritik033@gmail.com>
2025-10-29 16:19:50 +0530

    fix(hero): resolved image and height issue when using next/image with fill, add aspect ratio-[16/9]

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -5,6 +5,9 @@ import pagesData from "@/data/page.json";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import NotFoundPage from "../not-found";
+import Hero from "@/components/hero/Hero";
+import { centerHero, formHero, shapeDividerHero, splitHero, statsHero, videoHero } from "@/components/hero/defaults";
+import StatsVariant from "@/components/hero/variants/StatsVariant";
 
 export default function ClientPage({ params }: { params: { slug?: string[] } }) {
   const session = useSession();
@@ -45,7 +48,12 @@ export default function ClientPage({ params }: { params: { slug?: string[] } }) 
       <h1>Editor - TDACorp</h1>
       <pre>{JSON.stringify(session, null, 2)}</pre>
 
-     
+     <Hero schema={splitHero}/>
+     <Hero schema={centerHero}/>
+     <Hero schema={formHero}/>
+     <Hero schema={shapeDividerHero}/>
+     <Hero schema={statsHero}/>
+     <Hero schema={videoHero}/>
     </main>
   );
 }
