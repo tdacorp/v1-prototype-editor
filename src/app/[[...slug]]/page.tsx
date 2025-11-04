da903d9c701087a9aee43718d1d0e2d2b9e9514f
mohitrajgarh <mohitsainirajgarh@gmail.com>
2025-11-04 10:44:21 +0530

    fix(slug): fix the build error and remove usesession

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,13 +1,7 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
 import pagesData from "@/data/page.json";
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
 import NotFoundPage from "../not-found";
 
-export default function ClientPage({ params }: { params: { slug?: string[] } }) {
-  const session = useSession();
+export default async function Page({ params }: { params: { slug?: string[] } }) {
   const slugArray = Array.isArray(params?.slug) ? params.slug : [];
   const fullPath = slugArray.join("/");
 
@@ -16,36 +10,13 @@ export default function ClientPage({ params }: { params: { slug?: string[] } }) 
   if (!page) {
     return (
       <main className="p-8">
-        <div className="flex justify-end mb-4">
-          {session.status === "authenticated" ? (
-            <Button onClick={() => signOut()}>Sign out</Button>
-          ) : (
-            <Button asChild>
-              <Link href="/auth">Sign in</Link>
-            </Button>
-          )}
-        </div>
         <NotFoundPage />
       </main>
     );
   }
-
   return (
-    <main className="p-6">
-      <div className="flex justify-end">
-        {session.status === "authenticated" ? (
-          <Button onClick={() => signOut()}>Sign out</Button>
-        ) : (
-          <Button asChild>
-            <Link href="/auth">Sign in</Link>
-          </Button>
-        )}
-      </div>
-
-      <h1>Editor - TDACorp</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-
-     
+    <main className="p-6 mt-9">
+     <h1>Welcome to the Editor</h1>
     </main>
   );
 }
